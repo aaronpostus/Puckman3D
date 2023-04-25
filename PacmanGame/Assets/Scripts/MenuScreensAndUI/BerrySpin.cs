b7149207399edbd436ece2e27fe91c11b1259c05
Aaron Post <aarontpost@gmail.com>
2023-04-24 23:05:38 -0400

    Added namespaces

--- PacmanGame/Assets/Scripts/MenuScreensAndUI/BerrySpin.cs
+++ PacmanGame/Assets/Scripts/MenuScreensAndUI/BerrySpin.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BerrySpin : MonoBehaviour
-{
-    [SerializeField] GameObject berry;
-    [SerializeField] float rotationSpeed;
-    [SerializeField] Vector3 rotationAxis;
-    private Transform transform;
-
-    // Start is called before the first frame update
-    void Start()
+namespace OttiPostLewis.Lab6 {
+    public class BerrySpin : MonoBehaviour
     {
-        this.transform = berry.transform;
-    }
+        [SerializeField] GameObject berry;
+        [SerializeField] float rotationSpeed;
+        [SerializeField] Vector3 rotationAxis;
+        private Transform transform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.RotateAround(transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.transform = berry.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.RotateAround(transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
+        }
     }
 }
--- PacmanGame/Assets/Scripts/MenuScreensAndUI/HoverAnimation.cs
+++ PacmanGame/Assets/Scripts/MenuScreensAndUI/HoverAnimation.cs
@@ -1,40 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HoverAnimation : MonoBehaviour
-{
-    [SerializeField] GameObject gameObj;
-    [SerializeField] float height;
-    [SerializeField] float speed;
-    private bool direction;
-    private Transform transform;
-    private float initialPosition;
-    // Start is called before the first frame update
-    void Start()
+namespace OttiPostLewis.Lab6 {
+    public class HoverAnimation : MonoBehaviour
     {
-        this.transform = gameObj.transform;
-        this.direction = (Random.Range(0,2) == 0);
-        this.initialPosition = transform.position.y;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(direction) {
-            if(transform.position.y > initialPosition + height) {
-                direction = !direction;
-            }
-            else {
-                transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime), transform.position.z);
-            }
+        [SerializeField] GameObject gameObj;
+        [SerializeField] float height;
+        [SerializeField] float speed;
+        private bool direction;
+        private Transform transform;
+        private float initialPosition;
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.transform = gameObj.transform;
+            this.direction = (Random.Range(0,2) == 0);
+            this.initialPosition = transform.position.y;
         }
-        else {
-            if(transform.position.y < initialPosition - height) {
-                direction = !direction;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(direction) {
+                if(transform.position.y > initialPosition + height) {
+                    direction = !direction;
+                }
+                else {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime), transform.position.z);
+                }
             }
             else {
-                transform.position = new Vector3(transform.position.x, transform.position.y - (speed * Time.deltaTime), transform.position.z);
+                if(transform.position.y < initialPosition - height) {
+                    direction = !direction;
+                }
+                else {
+                    transform.position = new Vector3(transform.position.x, transform.position.y - (speed * Time.deltaTime), transform.position.z);
+                }
             }
         }
     }
